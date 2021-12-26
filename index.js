5a7f080bb2244082bbb46713fe1fbcdcb6d36d78
welllee <l1wei@protonmail.com>
2021-12-26 22:18:11 +0800

    test

---
+++ index.js
@@ -0,0 +1,48 @@
+const postcss = require('postcss');
+const fs = require('fs');
+const path = require('path')
+
+const abcCssPlugin = (opts => {
+    opts = opts || {};
+    // const { colorMap } = opts;
+
+    console.log('@@@')
+    // console.log(opts)
+    return {
+        postcssPlugin: 'postcss-abc',
+        Rule (rule) {
+            // console.log(rule.nodes)
+            findColorValue(rule)
+        }
+    }
+});
+
+const color = ['#2c3e50', '#42b983']
+
+function findColorValue(rule) {
+    rule.nodes.forEach(o => {
+        if (o.type === 'decl') {
+            color.forEach(c => {
+                if (o.value && o.value.includes(c)) {
+                    console.log(`${o.parent.selector} - ${o.prop}: warn, the value ${o.value} in theme`)
+                }
+            })
+        }
+    })
+}
+
+
+
+
+
+const scss = fs.readFileSync('./src/assets/style.scss');
+
+postcss([abcCssPlugin({
+
+})]).process(scss).then(result => {
+    // console.log(result);
+});
+
+module.exports = {
+    abcCssPlugin
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div class="wrapper">test</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import HelloWorld from '@/components/HelloWorld.vue'
+import '../assets/style.scss'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
